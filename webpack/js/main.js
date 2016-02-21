cdf5bc5028fbc497ec15ba8c13f9fa9522cb3ef6
Raphael Pierzina <raphael@hackebrot.de>
2016-02-21 00:42:58 +0000

    Change bootswatch theme to yeti

--- webpack/js/main.js
+++ webpack/js/main.js
@@ -2,6 +2,6 @@ var $ = require('jquery');
 window.$ = $;
 window.jQuery = $;
 require("bootstrap");
-require("!style!css!bootswatch/lumen/bootstrap.min.css");
+require("!style!css!bootswatch/yeti/bootstrap.min.css");
 
 
