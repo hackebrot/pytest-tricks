b634268497616e6e25e47557499de7b259d1615c
Raphael Pierzina <raphael@hackebrot.de>
2016-03-01 21:42:30 +0000

    Implement fizzbuzz to be used on the examples

--- code_examples/create_tests_via_parametrization/test_parametrize.py
+++ code_examples/create_tests_via_parametrization/test_parametrize.py
@@ -2,9 +2,29 @@
 
 import pytest
 
-@pytest.mark.parametrize('foo, bar', [
-    ('foo1', 'bar1'),
-    ('foo2', 'bar2')
-])
-def test_foobar(foo, bar):
-    assert True
+RULES = (
+    (3 * 5, "FizzBuzz"),
+    (3, "Fizz"),
+    (5, "Buzz"),
+)
+
+
+def fizzbuzz(number):
+    for div_number, substitution in RULES:
+        if not number % div_number:
+            return substitution
+    return str(number)
+
+
+@pytest.mark.parametrize(
+    'number, word', [
+        (1, '1'),
+        (3, 'Fizz'),
+        (5, 'Buzz'),
+        (10, 'Buzz'),
+        (15, 'FizzBuzz'),
+        (16, '16')
+    ]
+)
+def test_fizzbuzz(number, word):
+    assert fizzbuzz(number) == word
